074ec91b020c850d375bb4dab6249c7734adf6d7
yoonisali <yoonismahamoudali@gmail.com>
2022-12-13 09:20:29 -0800

    intial commit
    co-authored-by: Brandon Fenk <brandon.fenk99@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectName // Change project name!
+namespace ToDoList // Change project name!
 {
   class Program
   {
